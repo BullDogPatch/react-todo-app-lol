9ac62920068f73f7bbc5a9aac3d721882e2beb87
Craig Clayton <craig__clayton@outlook.com>
2024-03-15 16:16:16 +0000

    revert back

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -4,9 +4,7 @@ function TodoItem({ item }) {
   return (
     <>
       <div className={styles.todoitemContainer}>
-        <h3 className={styles.todoTitle}>{item}</h3>
-        {/* <button>Delete</button> */}
-        {/* <hr className={styles.seprator} /> */}
+        <div className={styles.todoTitle}>{item}</div>
       </div>
     </>
   );
